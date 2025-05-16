f4f3cc655b922c6b34805a5d77f446b211de0f44
vinctuan02 <vinctuan02@gmail.com>
2025-05-16 11:13:10 +0700

    feat(jwt): config jwt

---
+++ src/jwt/jwt.module.ts
@@ -0,0 +1,14 @@
+// import { Module } from '@nestjs/common';
+// import { JwtModule } from '@nestjs/jwt';
+
+// @Module({
+// 	imports: [
+// 		JwtModule.register({
+// 			secret: process.env.JWT_SECRET || 'your_default_secret',
+// 			signOptions: { expiresIn: '1h' },
+// 		}),
+// 	],
+// 	providers: [JwtServiceCustom],
+// 	exports: [JwtServiceCustom],
+// })
+// export class JwtAuthModule {}
